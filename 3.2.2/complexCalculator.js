47e89dfa14573bfe76f0f666d18481d0695a9480
dimitriDries <59876540+dimitriDries@users.noreply.github.com>
2020-02-19 22:31:33 +0100

    3.2.4

--- 3.2.2/complexCalculator.js
+++ 3.2.2/complexCalculator.js
@@ -2,7 +2,7 @@ console.log("complexCalculator.js loaded succesfully");
 var calculationArray = [];
 
 //add clicked number in field AND add number to array
-function addNumber (clickedNumber) {
+function addNumber () {
     //Loop verwijderd de eventlisteners van alle nummer buttons en zet vervolgens een eventlistener erop (addNumber)
     var numButton = document.getElementsByClassName("num_btn");
     for (var i=0; i<numButton.length; i++){
@@ -11,19 +11,34 @@ function addNumber (clickedNumber) {
    }
 
     var calculationField = document.getElementById("calculationField");
-    calculationField.textContent += clickedNumber;
+    calculationField.textContent += event.target.innerHTML;
 }
+
+//Automatically add functions to numbtn's
+(function (){
+    var numButton = document.getElementsByClassName("num_btn");
+    for (i=0; i<numButton.length; i++){
+        numButton[i].addEventListener("click", addNumber);
+    }
+})();
 
 function calculate () {
     console.log(calculationArray.join(''));
 }
 
+function clearField() {
+    var calculationField = document.getElementById("calculationField");
+    calculationField.textContent="";
+}
 
 function plus () {
 //1. class aanpassen naar blijvend donkerder
 //2. + wegschrijven in array
 //3. Bij eerst volgende nummer geselecteerd: view clearen en overschrijven door aangeklikte nummer
-
+    var numButton = document.getElementsByClassName("num_btn");
+    for (i=0; i<numButton.length; i++){
+        numButton[i].addEventListener("click", clearField);
+    }
         var valueOne = calculationField.textContent;
         var operator = "+";
         return calculationArray.push(valueOne) + calculationArray.push(operator);
@@ -38,12 +53,6 @@ function multiply () {
 }
 
 function divide () {
-
-}
-
-function clearField() {
-    var calculationField = document.getElementById("calculationField");
-    calculationField.textContent="";
 
 }
 
