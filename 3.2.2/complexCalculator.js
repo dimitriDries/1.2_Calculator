c171f85985969be349b30da67713496479aa82e7
dimitriDries <59876540+dimitriDries@users.noreply.github.com>
2020-02-29 20:17:31 +0100

    1.2.3

--- 3.2.2/complexCalculator.js
+++ 3.2.2/complexCalculator.js
@@ -1,33 +1,35 @@
 console.log("complexCalculator.js loaded succesfully");
-var calculationArray = [];
+var numbersArray = [];
+var operatorArray = [];
+
+var calculationField = document.getElementById("calculationField");
+
+var numButton = document.getElementsByClassName("num_btn");
+var operatorButton = document.getElementsByClassName("operator")
+
+var plusButton = document.getElementById("plus");
+var minusButton = document.getElementById("minus");
+
 
 //add clicked number in field AND add number to array
 function addNumber () {
     //Loop verwijderd de eventlisteners van alle nummer buttons en zet vervolgens een eventlistener erop (addNumber)
-    var numButton = document.getElementsByClassName("num_btn");
     for (var i=0; i<numButton.length; i++){
         var toClear = numButton.item(i);
-        toClear.removeEventListener("click", clearField);
+        toClear.removeEventListener("mousedown", clearField);
    }
-
-    var calculationField = document.getElementById("calculationField");
+   //Adds the clicked number to the view
     calculationField.textContent += event.target.innerHTML;
 }
 
 //Automatically add functions to numbtn's
 (function (){
-    var numButton = document.getElementsByClassName("num_btn");
     for (i=0; i<numButton.length; i++){
         numButton[i].addEventListener("click", addNumber);
     }
 })();
 
-function calculate () {
-    console.log(calculationArray.join(''));
-}
-
 function clearField() {
-    var calculationField = document.getElementById("calculationField");
     calculationField.textContent="";
 }
 
@@ -35,25 +37,34 @@ function plus () {
 //1. class aanpassen naar blijvend donkerder
 //2. + wegschrijven in array
 //3. Bij eerst volgende nummer geselecteerd: view clearen en overschrijven door aangeklikte nummer
-    var numButton = document.getElementsByClassName("num_btn");
+    plusButton.classList = "operatorActive";
     for (i=0; i<numButton.length; i++){
-        numButton[i].addEventListener("click", clearField);
+        numButton[i].addEventListener("mousedown", clearField);
     }
         var valueOne = calculationField.textContent;
         var operator = "+";
-        return calculationArray.push(valueOne) + calculationArray.push(operator);
+        return numbersArray.push(valueOne) + operatorArray.push(operator);
 }
 
 function minus () {
-
 }
 
 function multiply () {
-
 }
 
 function divide () {
+}
 
+function calculate () {
+    var valueTwo = calculationField.textContent;
+    numbersArray.push(valueTwo);
+    var calculation = numbersArray[0]+operatorArray[0]+numbersArray[1];
+
+    var activeOperator = document.getElementsByClassName("operatorActive");
+    activeOperator[0].classList = "operator";
+
+    console.log(calculation);
+    calculationField.textContent = eval(calculation);
 }
 
 //Opties: 
