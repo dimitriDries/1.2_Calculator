3fe8795698bb83a288787919bee49efa6c92d8dd
dimitriDries <59876540+dimitriDries@users.noreply.github.com>
2020-03-02 08:15:50 +0100

    1.2.5

--- 3.2.2/complexCalculator.js
+++ 3.2.2/complexCalculator.js
@@ -1,17 +1,27 @@
 console.log("complexCalculator.js loaded succesfully");
+
+// optional: Object instead of 2 arrays
+//var calculatorObject = {
+//    numbers: [],
+//    operator: []
+//}
+
 var numbersArray = [];
 var operatorArray = [];
 
-var calculationField = document.getElementById("calculationField");
+var view = document.getElementById("view");
 
 var numButton = document.getElementsByClassName("num_btn");
 var operatorButton = document.getElementsByClassName("operator")
 
+//declare secondary operators
 var plusButton = document.getElementById("plus");
 var minusButton = document.getElementById("minus");
+//declare primary operators
+var multiplyButton = document.getElementById("multiply");
+var divideButton = document.getElementById("divide");
 
-
-//add clicked number in field AND add number to array
+//add clicked number in field
 function addNumber () {
     //Loop verwijderd de eventlisteners van alle nummer buttons en zet vervolgens een eventlistener erop (addNumber)
     for (var i=0; i<numButton.length; i++){
@@ -19,52 +29,131 @@ function addNumber () {
         toClear.removeEventListener("mousedown", clearField);
    }
    //Adds the clicked number to the view
-    calculationField.textContent += event.target.innerHTML;
+    view.textContent += event.target.innerHTML;
 }
 
-//Automatically add functions to numbtn's
-(function (){
-    for (i=0; i<numButton.length; i++){
-        numButton[i].addEventListener("click", addNumber);
-    }
-})();
-
 function clearField() {
-    calculationField.textContent="";
+    //Clear view
+    view.textContent="";
+    //Clear active operator class
+    var activeOperator = document.getElementsByClassName("operatorActive");[0];
+    if (activeOperator !== undefined) {activeOperator.classList = "operator";}
+}
+
+function clearAll() {
+    //Clear view
+    view.textContent="";
+    //Clear active operator class
+    var activeOperator = document.getElementsByClassName("operatorActive");[0];
+    if (activeOperator !== undefined) {activeOperator.classList = "operator";}
+    //Clear arrays
+    numbersArray.length = 0;
+    operatorArray.length = 0;
 }
 
 function plus () {
-//1. class aanpassen naar blijvend donkerder
-//2. + wegschrijven in array
-//3. Bij eerst volgende nummer geselecteerd: view clearen en overschrijven door aangeklikte nummer
-    plusButton.classList = "operatorActive";
-    for (i=0; i<numButton.length; i++){
-        numButton[i].addEventListener("mousedown", clearField);
+    //--------- CSS changes start -------------------------------------------------------
+    var activeOperator = document.getElementsByClassName("operatorActive");
+    //If there is no active operator yet, clear field when next number is pressed
+    if (activeOperator.length === 0){
+        for (i=0; i<numButton.length; i++){
+            numButton[i].addEventListener("mousedown", clearField);
+        }
     }
-        var valueOne = calculationField.textContent;
-        var operator = "+";
-        return numbersArray.push(valueOne) + operatorArray.push(operator);
+
+    //if there is at least one active operator, reset class of that operator
+    if (activeOperator[0] !== undefined) {activeOperator.classList = "operator";}
+
+    //activate this operator
+    plusButton.classList = "operatorActive";
+    //---------- CSS changes end ------------------------------------------------------------------
+
+    //---------- Array changes start ------------------------------------------------------------------
+    var newValue = view.textContent;
+    var operator = "+";
+    numbersArray.push(newValue) + operatorArray.push(operator);
+    console.log(numbersArray[numbersArray.length-2]+operator+numbersArray[numbersArray.length-1]);
+    //---------- Array changes end ------------------------------------------------------------------
+
+    //---------- Calculation/ view + array update start ------------------------------------------------------------------
+    if (numbersArray.length>1){
+        view.textContent = eval(numbersArray[numbersArray.length-2]+operatorArray[this.length]+numbersArray[numbersArray.length-1]);
+        numbersArray.push(view.textContent);
+    }
+    console.log(numbersArray, operatorArray);
+    //---------- Calculation + array update end ------------------------------------------------------------------
+
 }
 
 function minus () {
+    //--------- CSS changes start -------------------------------------------------------
+    var activeOperator = document.getElementsByClassName("operatorActive");
+    //If there is no active operator yet, clear field when next number is pressed
+    if (activeOperator.length === 0){
+        for (i=0; i<numButton.length; i++){
+            numButton[i].addEventListener("mousedown", clearField);
+        }
+    }
+
+    //if there is at least one active operator, reset class of that operator
+    if (activeOperator[0] !== undefined) {activeOperator.classList = "operator";}
+
+    //activate this operator
+    minusButton.classList = "operatorActive";
+    //---------- CSS changes end ------------------------------------------------------------------
+
+    //---------- Array changes start ------------------------------------------------------------------
+    var newValue = view.textContent;
+    var operator = "-";
+    numbersArray.push(newValue) + operatorArray.push(operator);
+    console.log(numbersArray[numbersArray.length-2]+operator+numbersArray[numbersArray.length-1]);
+    //---------- Array changes end ------------------------------------------------------------------
+
+    //---------- Calculation/ view + array update start ------------------------------------------------------------------
+    if (numbersArray.length>1){
+        view.textContent = eval(numbersArray[numbersArray.length-2]+operatorArray[this.length]+numbersArray[numbersArray.length-1]);
+        numbersArray.push(view.textContent);
+    }
+    console.log(numbersArray, operatorArray);
+    //---------- Calculation + array update end ------------------------------------------------------------------
 }
 
 function multiply () {
+        //--------- CSS changes start -------------------------------------------------------
+        var activeOperator = document.getElementsByClassName("operatorActive");
+        //If there is no active operator yet, clear field when next number is pressed
+        if (activeOperator.length === 0){
+            for (i=0; i<numButton.length; i++){
+                numButton[i].addEventListener("mousedown", clearField);
+            }
+        }
+    
+        //if there is at least one active operator, reset class of that operator
+        if (activeOperator[0] !== undefined) {activeOperator.classList = "operator";}
+    
+        //activate this operator
+        multiplyButton.classList = "operatorActive";
+        //---------- CSS changes end ------------------------------------------------------------------
+    
+        //---------- Array changes start ------------------------------------------------------------------
+        var newValue = view.textContent;
+        var operator = "-";
+        numbersArray.push(newValue) + operatorArray.push(operator);
+        console.log(numbersArray[numbersArray.length-2]+operator+numbersArray[numbersArray.length-1]);
+        //---------- Array changes end ------------------------------------------------------------------
+    
 }
 
 function divide () {
 }
 
 function calculate () {
-    var valueTwo = calculationField.textContent;
+    var valueTwo = view.textContent;
     numbersArray.push(valueTwo);
-    var calculation = numbersArray[0]+operatorArray[0]+numbersArray[1];
-
-    var activeOperator = document.getElementsByClassName("operatorActive");
-    activeOperator[0].classList = "operator";
+    var calculation = numbersArray[numbersArray.length-2]+operatorArray[operatorArray.length-1]+numbersArray[numbersArray.length-1];
 
     console.log(calculation);
-    calculationField.textContent = eval(calculation);
+    view.textContent = eval(calculation);
 }
 
 //Opties: 
